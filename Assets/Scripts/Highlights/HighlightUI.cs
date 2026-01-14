31c964b93357d1ce48212bc47415c714d39390ae
Heather <Heathbeau@gmail.com>
2026-01-14 22:03:50 +0000

    Main Menu Almost Complete, Hightlight UI script made, & PSX shader working

---
+++ Assets/Scripts/Highlights/HighlightUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HighlightUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject highlight;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        highlight.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        highlight.SetActive(false);;
+    }
+}
