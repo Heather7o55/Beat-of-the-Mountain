a2f42c9189ad25dbe341de1a5f765f2fc37b0462
Heather <heath.beaumont.540@accesscreative.ac.uk>
2026-01-19 11:52:14 +0000

    bleeeh slight folder and code cleanup

--- Assets/Scripts/Dialogue Script.cs
+++ Assets/Scripts/Dialogue Script.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class DialogueScript : MonoBehaviour
 {
-    
+    void Update()
+    {
+        
+    }
 }
--- Assets/Scripts/Highlights/HighlightUI.cs
+++ Assets/Scripts/Highlights/HighlightUI.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-
+/* 
+    This is a generic, apply to the ui button, create a duplicate of the object you want to be highlighted, squash and stretch it, 
+    apply the highlight material, attach to this script, then disable it. 
+*/
 public class HighlightUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject highlight;
+    [SerializeField] private GameObject highlight;
     public void OnPointerEnter(PointerEventData eventData)
     {
         highlight.SetActive(true);
